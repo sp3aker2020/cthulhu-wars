60b3feb3998170d42cd101c67ef1c04604eda5c5
sp3aker2020 <sp3aker2020@users.noreply.github.com>
2026-08-05 10:06:41 +0300

    fix: polyfill Buffer for Anchor IDL parsing

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+
+export default defineConfig({
+  plugins: [
+    nodePolyfills({
+      include: ['buffer'],
+      globals: {
+        Buffer: true,
+      },
+    }),
+  ],
+  define: {
+    'process.env': {},
+  }
+});
